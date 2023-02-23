1cbe476ed9101c1677913f1dfd17a96adb8d6b82
NTh-Hai <thehai318@gmail.com>
2023-02-23 14:54:59 +0700

    updated UI

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -25,13 +25,19 @@ export const Content = () => {
     },
   });
 
+  const deleteTopic = api.topic.delete.useMutation({
+    onSuccess: () => {
+      void refetchTopics();
+    },
+  });
+
   return (
     <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
       <div className="px-2">
         <input
           type="text"
           placeholder="New Topic"
-          className="w-full border-spacing-1 border border-blue-800 "
+          className="w-full border-spacing-1 rounded-md border border-blue-800 p-1"
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               createTopic.mutate({
@@ -41,13 +47,16 @@ export const Content = () => {
             }
           }}
         />
-        <ul className="p-2">
+        <ul className="mt-3 flex flex-col gap-1 overflow-x-hidden">
           {topics?.map((topic) => (
-            <li key={topic.id} className="p-1">
+            <li
+              key={topic.id}
+              className={`group flex items-center rounded-md p-1  ${
+                topic == selectedTopic ? "selected-topic" : "hover:bg-gray-200"
+              }`}
+            >
               <Link
-                className={`block p-1 ${
-                  topic == selectedTopic ? "selected-topic" : ""
-                }`}
+                className="flex-1 p-1"
                 href="#"
                 onClick={(e) => {
                   e.preventDefault();
@@ -56,6 +65,16 @@ export const Content = () => {
               >
                 {topic.title}
               </Link>
+              <button
+                className="translate-x-20 cursor-pointer overflow-hidden rounded-md bg-red-500 p-1 opacity-0 transition-all duration-300 group-hover:translate-x-0 group-hover:opacity-100"
+                onClick={(e) =>
+                  deleteTopic.mutate({
+                    id: topic.id,
+                  })
+                }
+              >
+                Delete
+              </button>
             </li>
           ))}
         </ul>
